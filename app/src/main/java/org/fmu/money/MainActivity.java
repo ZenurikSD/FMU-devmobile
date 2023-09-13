d4319a1212848914b90b83912ef0a9f319150157
ZenurikSD <alissonsilva498@outlook.com>
2023-09-12 23:07:16 -0300

    Arquivos iniciais

---
+++ app/src/main/java/org/fmu/money/MainActivity.java
@@ -0,0 +1,31 @@
+package org.fmu.money;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText n1, n2, result;
+    private Button confirm_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        n1 = findViewById(R.id.val1);
+        n2 = findViewById(R.id.val2);
+        result = findViewById(R.id.result);
+    }
+
+    public void calculateResult(View view){
+        int x = Integer.parseInt(n1.getText().toString());
+        int y = Integer.parseInt(n2.getText().toString());
+
+        result.setText(Integer.toString(x + y));
+
+    }
+}
