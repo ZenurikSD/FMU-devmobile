09416bf6ffe63054f617b214a15cbefbf11a47bb
ZenurikSD <alissonsilva498@outlook.com>
2023-09-20 22:26:14 -0300

    Pequena reestruturação Activity adicionada e reordenação de pastas

---
+++ app/src/main/java/org/fmu/money/activities/Screen2.java
@@ -0,0 +1,16 @@
+package org.fmu.money.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.fmu.money.R;
+
+public class Screen2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen2);
+    }
+}
