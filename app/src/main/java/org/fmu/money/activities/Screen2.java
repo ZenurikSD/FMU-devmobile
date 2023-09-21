4d5bf272a8592a0397d554c39ba4721127ec1ebe
ZenurikSD <alissonsilva498@outlook.com>
2023-09-20 23:12:56 -0300

    Aula 18/09, segunda tela

--- app/src/main/java/org/fmu/money/activities/Screen2.java
+++ app/src/main/java/org/fmu/money/activities/Screen2.java
@@ -12,5 +12,6 @@ public class Screen2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen2);
+
     }
 }
