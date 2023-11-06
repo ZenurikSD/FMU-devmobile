e17f341f40a6a4986fcd390e7df3fd78bb6cdb34
zenu-cs50x-2023 <alisson.bwt@outlook.com>
2023-11-06 19:34:32 -0300

    Cansei

--- app/src/main/java/database/AlunoDao.java
+++ app/src/main/java/database/AlunoDao.java
@@ -1,6 +1,5 @@
 package database;
 
-
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
---
+++ app/src/main/java/network/Main2Activity.java
@@ -0,0 +1,16 @@
+package network;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.fmu.money.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
